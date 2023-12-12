19bd951600d422bc761d55e53f5f760b17d07200
Danil Iarosh <maze3290@gmail.com>
2023-12-12 22:21:43 +0300

    added 4th task of sem 2

--- sem_2/task_4.py
+++ sem_2/task_4.py
@@ -1,10 +1,26 @@
-from flask import Flask
+# Создать страницу, на которой будет форма для ввода текста и
+# кнопка "Отправить"
+# При нажатии кнопки будет произведен подсчет количества слов
+# в тексте и переход на страницу с результатом.
+
+
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
 
+@app.get('/')
+def index():
+    return render_template('form.html')
 
+
+@app.post('/')
+def index_post():
+    text = request.form.get('input')
+    length = len(text.split())
+    
+    return render_template('form.html', text=text, length=length)
 
 
 if __name__ == "__main__":
