07a1a7a3c4853b2580991c0048803e7e2c88b0c4
Danil Iarosh <maze3290@gmail.com>
2023-12-12 21:21:30 +0300

    added 1st task of sem 2

--- sem_2/task_1.py
+++ sem_2/task_1.py
@@ -1,10 +1,25 @@
-from flask import Flask
+# Создать страницу, на которой будет кнопка "Нажми меня", при
+# нажатии на которую будет переход на другую страницу с
+# приветствием пользователя по имени.
+
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+    data = {
+        'name': 'Данил'
+    }
+    return render_template('button.html', **data)
 
+
+@app.route('/greet/<name>')
+def greet(name: str):
+    print(url_for('greet', name='Danil'))
+    return render_template('name.html', context=name)
 
 
 if __name__ == "__main__":
