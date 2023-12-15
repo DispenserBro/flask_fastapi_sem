068e6de8e8902dba9bde0bd9a5cef2d5093edf37
Danil Iarosh <maze3290@gmail.com>
2023-12-15 20:23:22 +0300

    edited dependencies

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
-from sem_2.task_5 import app
-
+from sem_3.task_1 import app
+# from personal.example import app
 
 if __name__ == "__main__":
-    app.run()
+    app.run('localhost', port=80, debug=True)
