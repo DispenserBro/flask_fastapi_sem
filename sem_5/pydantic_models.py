b826864dded9a1a35cfabd276ded234ef068bff0
Danil Iarosh <maze3290@gmail.com>
2023-12-22 21:46:43 +0300

    finished task 2 of sem 5

--- sem_5/pydantic_models.py
+++ sem_5/pydantic_models.py
@@ -1,9 +1,17 @@
-# Создайте класс Task с полями id, title, description и status.
-
 from pydantic import BaseModel
 
+
+# Создайте класс Task с полями id, title, description и status.
 class Task(BaseModel):
     id: int
     title: str
     description: str | None = None
     status: bool = False
+
+
+# Создайте класс Movie с полями id, title, description и genre.
+class Movie(BaseModel):
+    id: int
+    title: str
+    description: str | None = None
+    genre: str
