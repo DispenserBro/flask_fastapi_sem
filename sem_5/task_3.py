9be3b43641e5bcefad961aac95e9aead11aa47db
Danil Iarosh <maze3290@gmail.com>
2023-12-22 22:28:18 +0300

    finished task 6 of sem 5

--- sem_5/task_3.py
+++ sem_5/task_3.py
@@ -6,19 +6,44 @@
 # Создайте маршрут для добавления нового пользователя
 # (методы POST, PUT, DELETE).
 # Реализуйте валидацию данных запроса и ответа.
+# Создайте маршрут для отображения списка пользователей (метод GET).
+# Реализуйте вывод списка пользователей через шаблонизатор Jinja.
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 from .pydantic_models import User
 
 
 app = FastAPI()
+templates = Jinja2Templates(directory='sem_5/templates')
 users: list[User] = []
 
 
-@app.get('/')
-async def index():
-    return users
+@app.get('/', response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(
+        'users.html', {'request': request, 'users': users}
+    )
+
+
+@app.post('/')
+async def add_user(
+    request: Request,
+    name: str = Form(...),
+    email: str = Form(...),
+    password: str = Form(...),
+    id: int = Form(...),
+):
+    user = User(
+        id=int(id),
+        name=name,
+        email=email,
+        password=password,
+    )
+    users.append(user)
+    return user
 
 
 @app.post('/users/')
@@ -32,13 +57,13 @@ async def update_user(user_id: int, new_user: User):
     filtered_users = [user for user in users if user.id == user_id]
     if not filtered_users:
         return {'updated': False}
-    
+
     user = filtered_users[0]
-    
+
     user.name = new_user.name
     user.email = new_user.email
     user.password = new_user.password
-    
+
     return {'updated': True, 'user': new_user}
 
 
@@ -47,9 +72,9 @@ async def delete_user(user_id: int):
     filtered_users = [user for user in users if user.id == user_id]
     if not filtered_users:
         return {'deleted': False}
-    
+
     user = filtered_users[0]
-    
+
     users.remove(user)
-    
+
     return {'deleted': True, 'user': user}
